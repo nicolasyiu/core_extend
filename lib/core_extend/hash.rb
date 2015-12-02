d3443adde43924bc925575c7a6dd913dd2d9054b
saxer <15201280641@qq.com>
2015-12-02 11:10:13 +0800

    no message

--- lib/core_extend/hash.rb
+++ lib/core_extend/hash.rb
@@ -1,11 +1,11 @@
 class Hash
-  def to_yaml(opts = {})
-    YAML::quick_emit(object_id, opts) do |out|
-      out.map(taguri, to_yaml_style) do |map|
-        sort.each do |k, v| # "sort" added
-          map.add(k, v)
-        end
-      end
-    end
-  end
+  # def to_yaml(opts = {})
+  #   YAML::quick_emit(object_id, opts) do |out|
+  #     out.map(taguri, to_yaml_style) do |map|
+  #       sort.each do |k, v| # "sort" added
+  #         map.add(k, v)
+  #       end
+  #     end
+  #   end
+  # end
 end
--- lib/core_extend/version.rb
+++ lib/core_extend/version.rb
@@ -1,3 +1,3 @@
 module CoreExtend
-  VERSION = '0.1.8'
+  VERSION = '0.1.9'
 end
